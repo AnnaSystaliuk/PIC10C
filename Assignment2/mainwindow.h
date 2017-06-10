c4349dc4bf6d8622d60785cf1e37206b7a0efd0a
Anna <anna.systaliuk@gmail.com>
2017-06-10 02:51:12 -0700

    Second push

--- Assignment2/mainwindow.h
+++ Assignment2/mainwindow.h
@@ -15,34 +15,6 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-private slots:
-
-    void on_horizontalSlider_valueChanged(int value);
-
-    void on_horizontalSlider_2_valueChanged(int value);
-
-    void on_horizontalSlider_3_valueChanged(int value);
-
-    void on_horizontalSlider_4_valueChanged(int value);
-
-    void on_horizontalSlider_5_valueChanged(int value);
-
-    void on_horizontalSlider_6_valueChanged(int value);
-
-    void on_horizontalSlider_7_valueChanged(int value);
-
-    void on_horizontalSlider_8_valueChanged(int value);
-
-    void on_horizontalSlider_9_valueChanged(int value);
-
-    void on_horizontalSlider_10_valueChanged(int value);
-
-    void on_horizontalSlider_11_valueChanged(int value);
-
-    void on_radioButton_pressed();
-
-    void calculate();
-
 private:
     Ui::MainWindow *ui;
 };
